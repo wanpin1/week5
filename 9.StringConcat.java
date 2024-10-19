74954f1467275f2e2bd256d758cc85356ff70dd2
wanpin1 <3520645209@qq.com>
2024-10-19 09:39:29 +0800

    Create 9.StringConcat.java

---
+++ 9.StringConcat.java
@@ -0,0 +1,10 @@
+public class StringConcat {
+    public static void main(String[] args){
+        // Three strings are concatenated
+        String message = "Welcome " + "to " + "Java";
+        // String Chapter is concatenated with number 2
+        String s = "Chapter" +2; // s becomes Chapter2
+        // String Supplement is concatenated with character B
+        String s1 = "Supplement"+ 'B'; // s1 becomes SupplementB
+    }
+}
