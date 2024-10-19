a1522a4e63948f8c9b257e132a9caa1af5861e8d
wanpin1 <3520645209@qq.com>
2024-10-19 09:44:06 +0800

    Create 16.Week5Task23.java

---
+++ 16/Week5Task23.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Week5Task23 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the length from the center of a pentagon to a vertex:");
+        double r = input.nextDouble();
+        double s = 2 * r * Math.sin(Math.PI / 5);
+        double area = (5 * Math.pow(s, 2)) / (4 * Math.tan(Math.PI / 5));
+        System.out.printf("The area of the pentagon is %.2f\n", area);
+        input.close();
+    }
+}
