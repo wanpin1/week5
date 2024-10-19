e5bf1f217ca88fb52e4e2bb85127328426b6377d
wanpin1 <3520645209@qq.com>
2024-10-19 09:26:38 +0800

    Create 8.GetCharFromString.java

---
+++ 8.GetCharFromString.java
@@ -0,0 +1,7 @@
+public class GetCharFromString{
+    public static void main(String[] args){
+        String message = "Welcome to Java";
+        System.out.println("The first character in message is"
+                + message.charAt(0));
+    }
+}
