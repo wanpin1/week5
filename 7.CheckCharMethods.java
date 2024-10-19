1a08fb94ee200890d3b942c9f55f11502fdce6d2
wanpin1 <3520645209@qq.com>
2024-10-19 09:26:18 +0800

    Create 7.CheckCharMethods.java

---
+++ 7.CheckCharMethods.java
@@ -0,0 +1,10 @@
+public class CheckCharMethods {
+    public static void main(String[] args){
+        System.out.println("isDigit('a') is " + Character.isDigit('a'));
+        System.out.println("isletter('a') is " + Character.isletter('a'));
+        System.out.println("isLowerase('a") is " + Character.islowerCase('a'));
+        System.out.println("isUpperCase('a') is " +Character.isUpperCase('a'));
+        System.out.println("toLowerCase('T") is " + Character.toLowerCase('T'));
+        System.out.println("toUpperCase('q') is " + Character.toUpperCase('q'));
+    }
+}
