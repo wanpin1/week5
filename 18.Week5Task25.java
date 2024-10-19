69c6e1d9e54087ccb1065ccbe01e3289eaf9e1fa
wanpin1 <3520645209@qq.com>
2024-10-19 09:53:37 +0800

    Create 18.Week5Task25.java

---
+++ 18.Week5Task25.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Week5Task25 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter three cities:");
+        String city1 = input.nextLine();
+        String city2 = input.nextLine();
+        String city3 = input.nextLine();
+        // 比较城市名字典序的逻辑未完善
+        input.close();
+    }
+}
