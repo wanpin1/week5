3065df2bf30e80d25114ebc212b256427e29000e
wanpin1 <3520645209@qq.com>
2024-10-19 09:40:37 +0800

    Create 11.ReadCharFromConsole.java

---
+++ 11.ReadCharFromConsole.java
@@ -0,0 +1,5 @@
+import java.util.Scanner;
+public class ReadCharFromConsole {
+    public.out.println("Interest is $"+ interest);
+    System.out.printf("Interest with two decimal point is $%4.2f", interest);
+}
