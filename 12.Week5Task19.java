5695981de7ac9bdd813ba1a7013c3121396f283a
wanpin1 <3520645209@qq.com>
2024-10-19 09:41:07 +0800

    Create 12.Week5Task19.java

---
+++ 12.Week5Task19.java
@@ -0,0 +1,7 @@
+public class Week5Task19 {
+    public static void main(String[] args) {
+        int count =5;
+        double amount =45.56;
+        System.out.printf ("count is %d and amount is  f", count, amount);
+    }
+}
